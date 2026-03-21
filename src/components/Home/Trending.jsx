ea42a43258469f9c27c0700f5cffc659c4b8ebe4
adnan-akhlas <mail.adnanakhlas@gmail.com>
2026-03-22 00:38:15 +0600

    feat: show 8 app in home page

--- src/components/Home/Trending.jsx
+++ src/components/Home/Trending.jsx
@@ -15,7 +15,7 @@ export default function Trending() {
         subtitle={"Explore All Trending Apps on the Market developed by us"}
       />
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {apps.map((app) => (
+        {apps.slice(0, 8).map((app) => (
           <AppCard key={app.id} app={app} />
         ))}
       </div>
